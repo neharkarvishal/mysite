a7391a59851dbad885c0829e388199bb31161be1
Vishal Neharkar <37950421+neharkarvishal@users.noreply.github.com>
2018-07-20 23:15:03 +0530

    Updates.

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,10 @@ class Post(models.Model):
     title   =   models.CharField(max_length=200)
     text    =   models.TextField()
     created_date    =   models.DateTimeField(default=timezone.now())
-    publised_date   =   models.DateTimeField(blank=True, null=True)
+    published_date   =   models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.publised_date  =   timezone.now()
+        self.published_date  =   timezone.now()
         self.save()
 
     def __str__(self):
